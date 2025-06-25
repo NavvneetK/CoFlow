5dbfe41187b26cffe81ace2efe9fa33de292c3dd
Navneet Kaur <navneet78141@gmail.com>
2025-06-25 13:15:24 +0530

    disabled ESLint check during build

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   typescript:{
     ignoreBuildErrors: true,
   },
+  eslint: {
+    ignoreDuringBuilds: true, // disables ESLint check during build
+  },
   /* config options here */
   images:{
     remotePatterns:[{protocol:'https', hostname: 'img.clerk.com'}]
